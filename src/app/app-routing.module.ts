c6248bfdbca0cb936f0a8397d091ac589c60b9d5
Andrea Guffi <andreaguffi92@gmail.com>
2020-12-27 15:21:05 +0100

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DetailComponent } from './components/detail/detail.component';
+import { HomeComponent } from './components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'detail', component: DetailComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents = [HomeComponent, DetailComponent]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
-
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './components/home/home.component';
-import { DetailComponent } from './components/detail/detail.component';
+//import { HomeComponent } from './components/home/home.component';
+//import { DetailComponent } from './components/detail/detail.component';
 import { HeaderComponent } from './common/header/header.component';
 import { FooterComponent } from './common/footer/footer.component';
 import { SearchPageComponent } from './components/search-page/search-page.component';
@@ -14,8 +12,9 @@ import { SearchPageComponent } from './components/search-page/search-page.compon
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    DetailComponent,
+    //HomeComponent,
+    //DetailComponent,
+    routingComponents,
     HeaderComponent,
     FooterComponent,
     SearchPageComponent
