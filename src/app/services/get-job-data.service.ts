c53ee45b901217d28442452bd18042e428e60aa9
Andrea Guffi <andreaguffi92@gmail.com>
2021-01-02 18:08:46 +0100

    okk

--- src/app/services/get-job-data.service.ts
+++ src/app/services/get-job-data.service.ts
@@ -10,7 +10,9 @@ import { Observable } from 'rxjs'
 export class GetJobDataService {
 
   //private _url: string = 'https://jsonplaceholder.typicode.com/posts'
-  private _url: string = 'https://www.themuse.com/api/public/jobs?page=10&descending=true'
+  //private _url: string = 'https://www.themuse.com/api/public/jobs?page=10&descending=true'
+
+  private _url: string = 'https://www.themuse.com/api/public/jobs?category=Engineering&page=1&location=Italy'
   constructor(private http: HttpClient) { }
 
   getJobData(): Observable<any> {
