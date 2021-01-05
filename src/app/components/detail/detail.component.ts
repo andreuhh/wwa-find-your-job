b3535b141c4757d4b1c957df609cdb694bc267ed
Andrea Guffi <andreaguffi92@gmail.com>
2021-01-05 21:24:20 +0100

    detail

--- src/app/components/detail/detail.component.ts
+++ src/app/components/detail/detail.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { expLevels } from '../../utils/expLev';
+import { categories } from '../../utils/categories';
+import { Observable } from 'rxjs'
+
 
 @Component({
   selector: 'app-detail',
@@ -8,12 +13,34 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class DetailComponent implements OnInit {
 
-  constructor(private route: ActivatedRoute) { }
+  detailJobResults: any
 
+  constructor(
+    private route: ActivatedRoute,
+    private http: HttpClient,
+  ) {
+    this.detailJobResults = new Object()
+  }
+
+  id
   ngOnInit(): void {
-    let id = this.route.snapshot.paramMap.get("id");
-    console.log(id)
+    this.id = this.route.snapshot.paramMap.get("id");
+    console.log(this.id)
     //this.detail = this.detailService.getDetail(id)
+    this.getDataFromApi()
+  }
+
+  getDetailData(): Observable<any> {
+    return this.http.get<any>(
+      'https://www.themuse.com/api/public/jobs/' + this.id
+    );
+  }
+
+  getDataFromApi() {
+    this.getDetailData().subscribe((data) => {
+      this.detailJobResults = data;
+      console.log(this.detailJobResults)
+    })
   }
 
 }
